92768846017fb6e6f4ae92e4f0d973b09396e257
Zexiang Yu <alexiang_yu@foxmail.com>
2025-08-07 14:14:52 +0800

    reverse+

--- app/(marketing)/marketing/page.tsx
+++ app/(marketing)/marketing/page.tsx
@@ -2,15 +2,16 @@ import { Heading } from "../_components/heading";
 import { Heroes } from "../_components/heroes";
 import { Footer } from "../_components/footer";
 
-export default function Marketing() {
+const MarketingPage = () => {
     return (
-      // <div className="text-red-500 font-bold text-center">HELLO</div>
-      <div className="min-h-full flex flex-col">
-        <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-          <Heading />
-          <Heroes />
+        <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6">
+                <Heading />
+                <Heroes />
+                <Footer />
+            </div>
         </div>
-          <Footer />
-      </div>
     );
-  }
+}
+
+export default MarketingPage;
